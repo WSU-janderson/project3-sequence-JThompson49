47d8598e5a5940c7815cd996a88adc12d4772662
JThompson49 <jack.thompson49@outlook.com>
2025-10-15 20:06:51 -0400

    Created the main constructor for Sequence to create everything. Filled out std::ostream& operator<< quickly so I can debug and do some working tests. (Forgot commit didnt go through so here is next). Made push_back method and created small test in SequenceDebug.cpp. Working!

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,7 +1,19 @@
 #include "Sequence.h"
 #include <iostream>
 
-Sequence::Sequence(size_t sz) {}
+Sequence::Sequence(size_t sz) : head(nullptr), tail(nullptr), numElts(0) {
+    for (size_t i = 0; i < sz; ++i) {
+        SequenceNode* newNode = new SequenceNode("");
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        ++numElts;
+    }
+}
 Sequence::Sequence(const Sequence& s) {}
 Sequence::~Sequence() {}
 Sequence& Sequence::operator=(const Sequence& s) { return *this; }
@@ -29,7 +41,20 @@ std::string Sequence::back() const { return ""; }
 bool Sequence::empty() const { return true; }
 size_t Sequence::size() const { return 0; }
 
-void Sequence::push_back(std::string item) {}
+void Sequence::push_back(std::string item) {
+    SequenceNode* newNode = new SequenceNode(item);
+
+    if (!head) {
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+    ++numElts;
+}
+
+
 void Sequence::pop_back() {}
 void Sequence::insert(size_t position, std::string item) {}
 void Sequence::erase(size_t position) {}
@@ -37,5 +62,13 @@ void Sequence::erase(size_t position, size_t count) {}
 void Sequence::clear() {}
 
 std::ostream& operator<<(std::ostream& os, const Sequence& s) {
+    os << "<";
+    SequenceNode* current = s.head;
+    while (current) {
+        os << current->item;
+        if (current->next) os << ", ";
+        current = current->next;
+    }
+    os << ">";
     return os;
 }
--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -7,11 +7,19 @@
  * SequenceDebug from the drop-down menu next to the Build (hammer icon) if it is on SequenceTestHarness
  */
 #include <iostream>
+#include <sstream>
+#include "Sequence.h"
 
 using namespace std;
 
 int main() {
-    cout << "Your project is ready to go!" << endl;
+    Sequence seq(3);
+    seq[0] = "A";
+    seq[1] = "B";
+    seq[2] = "C";
 
+    std::cout << "Start: " << seq << std::endl;
+    seq.push_back("D");
+    std::cout << "After push_back:  " << seq << std::endl;
     return 0;
 }
